57fd9a8bb484c9f25efa472131e2be184e541692
ayush-saklani <ayushsaklani04@gmail.com>
2022-12-12 21:13:42 +0530

    modified:   Q_07.c 	new file:   Q_07.exe

--- Q_07.c
+++ Q_07.c
@@ -1,53 +1,82 @@
 #include<stdio.h>
 #include<stdio.h>
-struct node
-{
+struct node{
     int data;
     struct node * right,*left;
 };
-struct node *create(struct node *head){
-
+void insert(struct node * head,int temp){
+    if(head==0){ head->data=temp;}
+    else if(temp>head->data){ insert(head->right,temp); }
+    else if(temp<head->data){ insert(head->left,temp); }
 }
-struct node * create(){
+int main(){
+    int temp;
+    struct node * head0=0;
+   // head->left=head->right=0;
+    printf("ola");
+    while (1)
+    {
+        printf("enter data");
+        scanf("%d",&temp);
+        insert(head0,temp);
+    }
+    printf("ola");
+    return 0;
+}
+/*
+This code works - by tempest.
+Write a C program to insert a double linked list by inserting nodes in such a way that the
+resultant linked list remains in ascending order.(do not use any sorting technique).
+
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int  data;
+    struct node *next;
+    struct node *prev;
+};
+void display(struct node * head){
+    struct node * temp2=head;
+    printf("\n");
+    while(temp2!=NULL){
+        printf("%d ",temp2->data);
+        temp2=temp2->next;
+    }
+}
+int count(struct node *head){
+    struct node *temp=head;
+    int counter0=0 ;
+    while(temp!=0){
+        counter0++;
+        temp=temp->next;
+    }
+    return counter0;
+}
+struct node * insert(){
     int choice=1;
     struct node * head=0;
     while(choice){
-        struct node *newnode=(struct node *)malloc (sizeof(struct node));
+        struct node *newnode=(struct node *)malloc(sizeof(struct node));
         newnode->next =newnode->prev=0;
         printf("Enter data ");
         scanf("%d",&newnode->data);
-        struct node *temp;
         if(head==0){
             head=newnode;
-            temp=head;
         }
-        else{
-            temp->next=newnode;
-            newnode->prev=temp;
-            temp=temp->next;
-        }
-        printf("Enter 0 to exit \nEnter 1 to continue");
-        scanf("%d",&choice);
-    }
-    return head ;
-}
-int main(){
-    return 0;
-}
-
-
-/*if(head==0){
+        else if(newnode->data<=head->data){
+            newnode->next=head;
+            head->prev=newnode;
             head=newnode;
         }
-        else if(newnode->data>head->data){
+        else if(newnode->data>=head->data){
             struct node *head3=head,*head2=0;
-            printf("working");
             while(head3->data   <   newnode->data){
                 head2=head3;
                 head3=head3->next;
-                //problem is that head3 is going into null after entering second element
+                if(head3==0){
+                    break;;
+                }
             }
-            printf("working");
             if(head2->next!=0){
                 head2->next=newnode;
                 head3->prev=newnode;
@@ -59,8 +88,14 @@ int main(){
                 head2->next=newnode;
             }
         }
-        else{
-            newnode->next=head;
-            head->prev=newnode;
-            head=newnode;
-        }*/
+        printf("Enter 0 to exit \nEnter 1 to continue");
+        scanf("%d",&choice);
+    }
+    return head ;
+}
+int main(){
+    struct node *head;
+    head=insert();
+    display(head);
+    return 0;
+}*/
