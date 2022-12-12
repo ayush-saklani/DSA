8f1f9585109d29f6d59b46636f871d1b2be011ae
ayush-saklani <ayushsaklani04@gmail.com>
2022-12-13 00:25:23 +0530

    modified:   Q_07.c

--- Q_07.c
+++ Q_07.c
@@ -1,8 +1,16 @@
+/*  incompldete code right now
+This code works - by tempest.
+Write a C program to create a binary search tree and perform following operations: 
+         1) Search a particular key.  
+         2) Delete a node from the tree. 
+         3) Find total number of leaf nodes  
+         4) Find height of a binary search tree 
+         5) Count total numbers of nodes from right hand side of root node 
+*/
 #include<stdio.h>
 #include<stdlib.h>
 int search=0,leaf_count=0,right_count=0;    //global counter for easy stuff
-struct node
-{
+struct node{
     int data;
     struct node * right,*left;
 };
@@ -62,6 +70,7 @@ int main(){
         printf("press 0 to exit \npress 1 for continue\n");
         scanf("%d",&choice);
     }
+    //bst creation completed
     printf("\n\n\n\n");
     inorder(root);
     while (1){
@@ -105,7 +114,87 @@ int main(){
         case 6:
             return 0;
         }
-        
-  
-    }    
+    }
 }
+/*
+below code is stolen from net and above is done by tempest ;)
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node Node;
+
+// Define the Tree Node here
+struct Node {
+    int value;
+    // Pointers to the left and right children
+    Node* left, *right;
+};
+
+
+Node* init_tree(int data) {
+    // Creates the tree and returns the
+    // root node
+    Node* root = (Node*) malloc (sizeof(Node));
+    root->left = root->right = NULL;
+    root->value = data;
+    return root;
+}
+
+Node* create_node(int data) {
+    // Creates a new node
+    Node* node = (Node*) malloc (sizeof(Node));
+    node->value = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+void free_tree(Node* root) {
+    // Deallocates memory corresponding
+    // to every node in the tree.
+    Node* temp = root;
+    if (!temp)
+        return;
+    free_tree(temp->left);
+    free_tree(temp->right);
+    if (!temp->left && !temp->right) {
+        free(temp);
+        return;
+    }
+}
+
+int tree_height(Node* root) {
+    // Get the height of the tree
+    if (!root)
+        return 0;
+    else {
+        // Find the height of both subtrees
+        // and use the larger one
+        int left_height = tree_height(root->left);
+        int right_height = tree_height(root->right);
+        if (left_height >= right_height)
+            return left_height + 1;
+        else
+            return right_height + 1;
+    }
+}
+
+int main() {
+    // Program to demonstrate finding the height of a Binary Tree
+
+    // Create the root node having a value of 10
+    Node* root = init_tree(10);
+    
+    // Insert nodes onto the tree
+    root->left = create_node(20);
+    root->right = create_node(30);
+    root->left->left = create_node(40);
+    root->left->right = create_node(50);
+
+    // Find the height of the tree
+    int height = tree_height(root);
+    printf("Height of the Binary Tree: %d\n", height);
+
+    // Free the tree!
+    free_tree(root);
+    return 0;
+}*/
