471c64a10a551e18dbd4cdd4e66d4e899d7f2b9e
ayush-saklani <ayushsaklani04@gmail.com>
2022-12-18 21:08:33 +0530

    new file:   binary_search.c 	new file:   binary_search.exe 	modified:   linear_sort.c 	new file:   linear_sort.exe

--- binary_search.c
+++ binary_search.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
-int search(int arr[], int N, int x){
-	for (int i = 0; i < N; i++)
-		if (arr[i] == x)
-			return i+1;
+int binarySearch(int arr[], int l, int r, int x){
+	if (r >= l) {
+		int mid = l + (r - l) / 2;
+
+		// If the element is present at the middle
+		// itself
+		if (arr[mid] == x)
+			return mid+1;
+
+		// If element is smaller than mid, then
+		// it can only be present in left subarray
+		if (arr[mid] > x)
+			return binarySearch(arr, l, mid - 1, x);
+
+		// Else the element can only be present
+		// in right subarray
+		return binarySearch(arr, mid + 1, r, x);
+	}
+
+	// We reach here when element is not
+	// present in array
 	return -1;
 }
 void display(int arr[], int size){
@@ -24,7 +41,7 @@ int main(void){
 	display(arr, n);    
     printf("Enter item to search -> ");
     scanf("%d",&x);
-	int result = search(arr, n, x);
+	int result = binarySearch(arr, 0, n - 1, x);
 	(result == -1)
 		? printf("Element is not present in array")
 		: printf("Element is present at index %d", result);
