8d2f5c6a53e7f531cd530de9364c659ed675f709
ayush-saklani <ayushsaklani04@gmail.com>
2022-12-25 02:42:47 +0530

    modified:   graph.c

--- graph.c
+++ graph.c
@@ -1,73 +1,98 @@
-#include <stdio.h>
-#include <stdlib.h>
-struct AdjListNode{
-	int dest;
-	struct AdjListNode* next;
+#include<stdio.h>
+#include<stdlib.h>
+struct graph{
+    int vertices ;
+    struct adjacency * array ;
 };
-struct AdjList{
-	struct AdjListNode* head;
+struct adjacency_node{
+    int data ;
+    struct adjacency_node * next;
 };
-struct Graph{
-	int V;
-	struct AdjList* array;
+struct adjacency{
+    struct adjacency_node * head;
 };
-struct AdjListNode* newAdjListNode(int dest){
-struct AdjListNode* newNode =(struct AdjListNode*)malloc(sizeof(struct AdjListNode));
-	newNode->dest = dest;
-	newNode->next = NULL;
-	return newNode;
+struct graph * create(int vertices){
+    struct graph *graphi=(struct graph * )malloc(sizeof(struct graph));
+    graphi->array  = (struct adjacency * )malloc(vertices*sizeof(struct adjacency));
+    graphi->vertices=vertices;
+    for (int i = 0; i < vertices; i++){
+        graphi->array[i].head=NULL;
+    }
+    return graphi;
 }
-struct Graph* createGraph(int V){
-	struct Graph* graph= (struct Graph*)malloc(sizeof(struct Graph));
-	graph->V = V;
-graph->array = (struct AdjList*)malloc(V * sizeof(struct AdjList));
-	int i;
-	for (i = 0; i< V; ++i)
-		graph->array[i].head = NULL;
-	return graph;
+void display(struct graph* graphi){
+    for (int i = 0; i < graphi->vertices; i++){
+        struct adjacency_node * temp= graphi->array[i].head;
+        printf("\nAdjacent members of %d th vertex \n",i);
+        while (temp){
+            printf("%d - " ,temp->data);
+            temp=temp->next;
+        }
+    }
 }
-void addEdge(struct Graph* graph, int src, int dest){
-	struct AdjListNode* check = NULL;
-	struct AdjListNode* newNode = newAdjListNode(dest);
-	if (graph->array[src].head == NULL){
-		newNode->next = graph->array[src].head;
-		graph->array[src].head = newNode;
-	}
-	else{
-		check = graph->array[src].head;
-		while (check->next != NULL){
-			check = check->next;
-		}
-		check->next = newNode;
-	}
-	newNode = newAdjListNode(src);
-	if (graph->array[dest].head == NULL){
-		newNode->next = graph->array[dest].head;
-		graph->array[dest].head = newNode;
-	}
-	else{
-		check = graph->array[dest].head;
-		while (check->next != NULL){
-			check = check->next;
-		}
-		check->next = newNode;
-}	}
-void printGraph(struct Graph* graph){	int v;
-	for (v = 0; v < graph->V; ++v){
-		struct AdjListNode* pCrawl = graph->array[v].head;
-		printf("\n Adjacency list of vertex %d\n head ", v);
-		while (pCrawl){	printf("-> %d", pCrawl->dest);
-			pCrawl = pCrawl->next;
-}	printf("\n");	}	}
+struct graph * addedge(struct graph * graphi,int source , int destination ){
+    if(source==destination){
+        struct adjacency_node * check=NULL;
+        struct adjacency_node * newnode =(struct adjacency_node * )malloc(sizeof(struct adjacency_node));
+        newnode->data = destination;
+        newnode->next=NULL;
+        if(graphi->array[source].head==NULL){
+            newnode->next=graphi->array[source].head;
+            graphi->array[source].head=newnode ;
+        }
+        else{
+            check=graphi->array[source].head;
+            while(check->next!=0){
+                check=check->next;
+            }
+            check->next=newnode;
+        }
+    }
+    else{
+        struct adjacency_node * check=NULL;
+        struct adjacency_node * newnode =(struct adjacency_node * )malloc(sizeof(struct adjacency_node));
+        newnode->data = destination;
+        newnode->next=NULL;
+        if(graphi->array[source].head==NULL){
+            graphi->array[source].head=newnode ;
+        }
+        else{
+            check=graphi->array[source].head;
+            while(check->next!=0){
+                check=check->next;
+            }
+            check->next=newnode;
+        }
+        struct adjacency_node * newnode2 =(struct adjacency_node * )malloc(sizeof(struct adjacency_node));
+        newnode2->data = source;
+        newnode2->next=NULL;
+        if(graphi->array[destination].head==NULL){
+            graphi->array[destination].head=newnode2 ;
+        }
+        else{
+            check=graphi->array[destination].head;
+            while(check->next!=0){
+                check=check->next;
+            }
+            check->next=newnode2;
+        }
+    }
+    return graphi;
+}
 int main(){
-	int V = 5;
-	struct Graph* graph = createGraph(V);
-	addEdge(graph, 0, 1);	addEdge(graph, 0, 4);	addEdge(graph, 1, 2);
-	addEdge(graph, 1, 3);	addEdge(graph, 1, 4);	addEdge(graph, 2, 3);
-	addEdge(graph, 3, 4);
-	printGraph(graph);
-	return 0;
+    int n,a,b,choice=1;
+    printf("enter number of vertices\n");
+    scanf("%d",&n);
+    struct graph * graphi;
+    graphi=create(n);
+    while (choice){
+        printf("Enter source and destination -> ");
+        scanf("%d",&a);
+        scanf("%d",&b);
+        graphi=addedge(graphi,a,b);
+        printf("press 0 to exit. press 1 to continue\n");
+        scanf("%d",&choice);
+    }
+    display(graphi);
+    return 0;
 }
-
-
-
